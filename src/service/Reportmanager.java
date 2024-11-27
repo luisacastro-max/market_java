f0e0f96cbc719a4542c9f7d4c8a81d347ff5ad5f
luisacastro-max <luisacastros.santos@gmail.com>
2024-11-26 21:11:34 -0300

    translation to english

--- src/service/Reportmanager.java
+++ src/service/Reportmanager.java
@@ -4,26 +4,26 @@ import market.product;
 import java.util.List;
 
 public class Reportmanager {
-	  public void gerarRelatorioVencimento(List<product> produtos, int dias) {
-	        System.out.println("Produtos a vencer em até " + dias + " dias:");
-	        for (product produto : produtos) {
-	            if (produto instanceof FoodUtensils) {
-	                FoodUtensils alimento = (FoodUtensils) produto;
-	                if (alimento.getValidade() <= dias) {
-	                    alimento.exibirDetalhes(); 
-	                }
-	            }
-	        }
 
-	
-}
-	  public void gerarRelatorioBaixaQuantidade(List<product> produtos, int quantidadeLimite) {
-	        System.out.println("Produtos com baixa quantidade (<= " + quantidadeLimite + " unidades):");
-	        for (product produto : produtos) {
-	            if (produto.getQuantidadeEstoque() <= quantidadeLimite) {
-	                produto.exibirDetalhes();
-	            }
-	        }
-	    }
+    public void generateExpirationReport(List<product> products, int days) {
+        System.out.println("Products expiring in up to " + days + " days:");
+        for (product product : products) {
+            if (product instanceof FoodUtensils) {
+                FoodUtensils food = (FoodUtensils) product;
+                if (food.getValidity() <= days) {
+                    food.displayDetails();
+                }
+            }
+        }
+    }
 
+    public void generateLowStockReport(List<product> products, int quantityLimit) {
+        System.out.println("Products with low stock (<= " + quantityLimit + " units):");
+        for (product product : products) {
+            if (product.getStockQuantity() <= quantityLimit) {
+                product.displayDetails();
+            }
+        }
+    }
 }
+
